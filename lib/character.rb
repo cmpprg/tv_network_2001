ebc6ba4fcd489c235e0553bdb273444292db36a4
Ryan Camp <cmpprg@icloud.com>
2020-02-11 09:30:46 -0700

    Create show class with attributes and passing tests

--- lib/character.rb
+++ lib/character.rb
@@ -1,10 +1,10 @@
 class Character
-  attr_accessor :name, :actor, :salary
+  attr_reader :name, :actor, :salary
 
   def initialize(**args)
     @name = args[:name]
     @actor = args[:actor]
     @salary = args[:salary]
   end
-  
+
 end
