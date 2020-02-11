cc4073e0b47dfed6c219119b63d72e49ae6a8ff5
Ryan Camp <cmpprg@icloud.com>
2020-02-11 09:56:23 -0700

    Create add show method with passing test

--- test/network_test.rb
+++ test/network_test.rb
@@ -1,6 +1,8 @@
 require "minitest/autorun"
 require "minitest/pride"
 require "./lib/network"
+require "./lib/character"
+require "./lib/show"
 
 class NetworkTest < Minitest::Test
   def test_it_exists
@@ -15,5 +17,20 @@ class NetworkTest < Minitest::Test
     assert_equal "NBC", nbc.name
     assert_equal [], nbc.shows
   end
+
+  def test_it_can_add_shows
+    nbc = Network.new("NBC")
+    michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
+    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+    knight_rider = Show.new("Knight Rider", "Glen Larson", [michael_knight, kitt])
+    leslie_knope = Character.new({name: "Leslie Knope", actor: "Amy Poehler", salary: 2_000_000})
+    ron_swanson = Character.new({name: "Ron Swanson", actor: "Nick Offerman", salary: 1_400_000})
+    parks_and_rec = Show.new("Parks and Recreation", "Michael Shur & Greg Daniels", [leslie_knope, ron_swanson])
+    nbc.add_show(knight_rider)
+    nbc.add_show(parks_and_rec)
+
+    assert_equal [knight_rider, parks_and_rec], nbc.shows
+  end
+
 
 end
